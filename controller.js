62a627994abd343add6b63fc9dc58f0dc5a52ba0
Alfanshter <alfanno8@gmail.com>
2021-01-10 06:35:24 +0700

    setting route di server js

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok("Aplikasi Rest API berjalan")
+    response.ok("Aplikasi Rest API berjalan",res)
 };
