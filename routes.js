2fad74c28c3c8b63c04de7f0bf27f99426d755bd
Alfanshter <alfanno8@gmail.com>
2021-01-10 07:13:54 +0700

    menambahkan post data

--- routes.js
+++ routes.js
@@ -10,4 +10,7 @@ module.exports = function(app){
 
         app.route('/tampil/:id')
         .get(jsonku.tampilberdasarkanid);
+
+        app.route('/tambah')
+        .post(jsonku.tambahmahasiswa);
 }
